4688e78e25895f7b0d583d22f9ecf65cf10c1747
jhoan <soporte.desarrollo@sycgroup.co>
2022-08-09 11:31:14 -0500

    initial commit

---
+++ controllers/correoController.js
@@ -0,0 +1,36 @@
+const { request, response } = require('express');
+const nodeMailer = require('nodemailer');
+
+const envioCorreo = (req = request, resp = response) => {
+  let body = req.body;
+
+  let config = nodeMailer.createTransport({
+    host:'smtp.gmail.com',
+    port:465,
+    secure: true,
+    auth:{
+      user: 'xisfoinfo@gmail.com',
+      pass: 'woklfmnioxswtqje'
+    }
+  });
+
+  const opciones = {
+    from: 'Solicitud de registro en xisfo',
+    subject: 'Hola! Me he contactado desde la pagina de xisfo, estos son mis datos!',
+    to: 'info@xisfo.com',
+    text:body.mensaje
+  };
+
+  config.sendMail(opciones, function(error,result){
+      if (error) return resp.json({ok:false,msg:error});
+        return resp.json({
+          ok:true,
+          msg:result
+        });
+      })
+
+}
+
+module.exports = {
+  envioCorreo
+}
